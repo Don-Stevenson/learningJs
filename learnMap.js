c7ac7ef100190343a0e6706750dc862b61c38a37
Don-Stevenson <dsteven1@gmail.com>
2020-03-03 23:39:10 -0500

    add reformitting objects with map

--- learnMap.js
+++ learnMap.js
@@ -5,9 +5,26 @@ const tryMap = arr => {
   return arr.map(element => element * 3);
 };
 
-console.log(tryMap(array));
+// console.log(tryMap(array));
 
 // map outside of a function
 const mappedArr = array.map(element => element * 3);
 
 // console.log(mappedArr);
+
+// map to reformat objects in an array
+
+let objArr = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+  { key: 4, value: 40 }
+];
+
+let reformattedArr = objArr.map(obj => { let reformattedObj = {}
+    reformattedObj[obj.key] = obj.value
+    return reformattedObj
+})
+
+console.log("reformated array of objects ", reformattedArr)
+console.log("original array of objects ", objArr)
