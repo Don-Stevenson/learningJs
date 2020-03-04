fd656597bf1c1f2a39ec44614b5ed630216fdd41
Don-Stevenson <dsteven1@gmail.com>
2020-03-03 23:56:26 -0500

    add sums content of an object

--- learningReduce.js
+++ learningReduce.js
@@ -1,4 +1,17 @@
-const array = [1, 2, 3];
-const reducedArray = (accumlator, currentvalue) => accumlator + currentvalue;
+// sums the contents in an array
+// const array = [1, 2, 3];
+// const reducedArray = (accumlator, currentvalue) => accumlator + currentvalue;
 
-console.log(array.reduce(reducedArray));
+// console.log(array.reduce(reducedArray, 10));
+
+// sums the contents in an object
+const arrOfObj = [{ a: 1 }, { a: 2 }, { a: 3 }];
+//reducer requires an initial value to be used
+const intialVal = 0;
+
+console.log(
+  arrOfObj.reduce(
+    (accumlator, currentValue) => accumlator + currentValue.a,
+    intialVal
+  )
+);
