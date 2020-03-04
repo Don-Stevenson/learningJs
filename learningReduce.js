d2325c3410861858a2077d170ba3d24c72e377a5
Don-Stevenson <dsteven1@gmail.com>
2020-03-04 00:03:01 -0500

    array flatten works with 1 level of nested array

--- learningReduce.js
+++ learningReduce.js
@@ -4,14 +4,29 @@
 
 // console.log(array.reduce(reducedArray, 10));
 
-// sums the contents in an object
-const arrOfObj = [{ a: 1 }, { a: 2 }, { a: 3 }];
-//reducer requires an initial value to be used
-const intialVal = 0;
+// // sums the contents in an object
+// const arrOfObj = [{ a: 1 }, { a: 2 }, { a: 3 }];
+// //reducer requires an initial value to be used
+// const intialVal = 10;
+
+// console.log(
+//   arrOfObj.reduce(
+//     (accumlator, currentValue) => accumlator + currentValue.a,
+//     intialVal
+//   )
+// );
+
+//flattening arrays with reduce
+const arrayToBeFlatted = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8]
+];
 
 console.log(
-  arrOfObj.reduce(
-    (accumlator, currentValue) => accumlator + currentValue.a,
-    intialVal
+  arrayToBeFlatted.reduce(
+    (accumulator, currentValue) => accumulator.concat(currentValue),
+    []
   )
 );
