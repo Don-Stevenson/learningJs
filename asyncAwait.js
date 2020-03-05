7dfca07e16d3a1d01615938bf7cb1c7d45333b8a
Don-Stevenson <dsteven1@gmail.com>
2020-03-05 00:22:27 -0500

    working async await for real object off the net

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,21 +1,38 @@
+// // set timeout function to mimic a server response
+// const fakeServerResponse = () => {
+//   setTimeout(function() {
+//     console.log("slowed by two seconds");
+//   }, 2 * 1000);
+// };
 
-// set timeout function to mimic a server response
-const fakeServerResponse = () => {
-  setTimeout(function() {
-    console.log("slowed by two seconds");
-  }, 2 * 1000);
-};
+// // console.log(fakeServerResponse());
 
-// console.log(fakeServerResponse());
+// // trying to use async await
+// async function doSomething() {
+//   try {
+//       let serverResponse = await fakeServerResponse();
+//       return serverResponse;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-// trying to use async await
-async function doSomething() {
-  try {
-      let serverResponse = await fakeServerResponse();
-      return serverResponse;
-  } catch (error) {
-    console.error(error);
-  }
+// console.log(doSomething());
+
+// following along with fun fun function
+const fetch = require("node-fetch");
+
+async function fetchAvatarUrl(userId) {
+  const response = await fetch(
+    `https://catappapi.herokuapp.com/users/${userId}`
+  );
+  const data = await response.json();
+
+  console.log(data.imageUrl);
+
+  //   return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+  // .then(response => response.json())
+  // .then(data => data.imageUrl);
 }
 
-console.log(doSomething());
+fetchAvatarUrl(123);
