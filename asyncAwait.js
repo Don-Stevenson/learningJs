582b0c27c36bfbd31b705989ec074dcdd7796507
Don-Stevenson <dsteven1@gmail.com>
2020-03-04 23:59:26 -0500

    update code to server response

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,16 +1,18 @@
+
+// set timeout function to mimic a server response
 const fakeServerResponse = () => {
   setTimeout(function() {
     console.log("slowed by two seconds");
   }, 2 * 1000);
-  return (sum = 2 + 2);
 };
 
-console.log(fakeServerResponse());
+// console.log(fakeServerResponse());
 
+// trying to use async await
 async function doSomething() {
   try {
-      let sum = await fakeServerResponse() + 2;
-      return sum;
+      let serverResponse = await fakeServerResponse();
+      return serverResponse;
   } catch (error) {
     console.error(error);
   }
