b124896d919aa893e523eafdf71a5f94b7227183
Don-Stevenson <dsteven1@gmail.com>
2020-03-04 23:45:52 -0500

    timeout working, but getting adding another +2 to the time out is not yet

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,16 +1,19 @@
-const delayTime = () => {
+const fakeServerResponse = () => {
   setTimeout(function() {
     console.log("slowed by two seconds");
-  }, 2 *1000);
+  }, 2 * 1000);
+  return (sum = 2 + 2);
 };
+
+console.log(fakeServerResponse());
 
 async function doSomething() {
   try {
-    let awaitTimeOut = await delayTime();
-    console.log("here", awaitTimeOut);
+      let sum = await fakeServerResponse() + 2;
+      return sum;
   } catch (error) {
     console.error(error);
   }
 }
 
-doSomething();
+console.log(doSomething());
